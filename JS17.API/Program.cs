f607a6eb17f025c466bff167f180fcf8e08af73b
Kirill Perepechkin <perepechkina.anastasiya.2002@yandex.ru>
2025-03-13 08:26:23 +0300

    enable swagger every env

--- JS17.API/Program.cs
+++ JS17.API/Program.cs
@@ -16,11 +16,8 @@ builder.Services.AddDbContext<WebDbContext>(x => x.UseSqlServer(connectString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
