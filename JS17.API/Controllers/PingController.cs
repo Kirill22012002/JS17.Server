5852bfce1e1dd7a02f3ae1a7880283440cfbfed2
Kirill Perepechkin <perepechkina.anastasiya.2002@yandex.ru>
2025-03-13 22:44:42 +0300

    added healtchecks for server and db

---
+++ JS17.API/Controllers/PingController.cs
@@ -0,0 +1,36 @@
+﻿using JS17.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JS17.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class PingController : ControllerBase
+{
+    private readonly WebDbContext _dbContext;
+
+    public PingController(WebDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public IActionResult PingServer()
+    {
+        return Ok("pong");
+    }
+
+    [HttpGet]
+    public IActionResult PingSqlServer()
+    {
+        var success = _dbContext.Database.CanConnect();
+        if(success)
+        {
+            return Ok("pong");
+        }
+        else
+        {
+            return BadRequest("cannot connect");
+        }
+    }
+}
